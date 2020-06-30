22b48bf5f2f81bacdd3a7b3e094c0af8ef16f50f
Rohit <rohitsay89@gmail.com>
2020-06-30 19:39:13 -0400

    added inheritance learn code

--- main.py
+++ main.py
@@ -4,13 +4,38 @@ import datetime
 print("Hello World, this is python learning codebase")
 print("Major version = ", sys.version_info.major,
       "\nMinor version = ", sys.version_info.minor)
-print("Date & Time = ", datetime.datetime.now())
+print("Run Date & Time = ", datetime.datetime.now())
 
+# simple function implementation:
 def add_func(x, y):
     print("This is a function for adding 2 nums")
-    #x = 10
-    #y = 20
     z = x+y
     print("Sum = ", z)
+#add_func(20, 20)
+print("Scope name = ", __name__)
 
-add_func(20, 20)
+# Sample class
+class Person:
+    # constructor:
+    def __init__(self):
+        self.name = input("Enter First name:")
+        self.age = input("Enter Age:")
+    def getName(self):
+        return self.name
+    def getAge(self):
+        return self.age
+
+class Student(Person):
+    def __init__(self):
+        Person.__init__(self)
+        self.rollNum = input("Enter Roll number:")
+        self.avgMarks = input("Enter average marks:")
+    def getRollNumber(self):
+        return self.rollNum
+    def getAvgMarks(self):
+        return self.avgMarks
+
+x = Student()
+#x = Person()
+print("Name = ", x.getName(), "Age = ", x.getAge())
+print("Roll number = ", x.getRollNumber(), "Average marks = ", x.getAvgMarks())
