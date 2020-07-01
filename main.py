dee29fdae56f90554f93c0e59fe6460055cc3b78
Rohit <rohitsay89@gmail.com>
2020-06-30 20:17:28 -0400

    Added iterators using class

--- main.py
+++ main.py
@@ -51,7 +51,25 @@ class GradStudent(Student):
             return "Not Employed"
 
 #x = Student()
-x = GradStudent()
-print("Name = ", x.getName(), "Age = ", x.getAge())
-print("Roll number = ", x.getRollNumber(), "Average marks = ", x.getAvgMarks())
-print("Subject = ", x.getSubject(), "Employment status = ", x.getEmploymentStatus())
+#x = GradStudent()
+#print("Name = ", x.getName(), "Age = ", x.getAge())
+#print("Roll number = ", x.getRollNumber(), "Average marks = ", x.getAvgMarks())
+#print("Subject = ", x.getSubject(), "Employment status = ", x.getEmploymentStatus())
+
+# iterators using classes
+class Reverse:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if(self.index == 0):
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+iter(rev)
+for char in rev:
+    print(char)
