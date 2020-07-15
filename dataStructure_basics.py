e264f634f7d3ecd663dfa1a9fff0c7102bce09b4
Rohit <rohitsay89@gmail.com>
2020-07-14 22:34:46 -0400

    Added function for using the stack data structure

--- dataStructure_basics.py
+++ dataStructure_basics.py
@@ -22,26 +22,31 @@ a = [0, 0, 0, 0, 0]  # create a default list of all zeros
 top = 0
 print("Program to create a stack data structure in Python using list \n")
 
+
 def Push(n):  # Push function: Push data on the stack
     global top  # specify the 'top' variable is global
     a[top] = n  # store the value at the top of stack
     top = top + 1  # increment the top
-    print
-    top  # print the top position number after each Push operation
+    print("Top = ", top)  # print the top position number after each Push operation
 
 
 def Pop():  # Pop function: remove data from top of stack
     global top  # specify the 'top' variable is global
     top = top - 1  # decrement the top variable
-    print
-    top  # print the top position number in array
+    print("Top = ", top)  # print the top position number after each Push operation
 
 
-Push(5)  # Push and Pop operation
-Push(6)
-Push(27)
-Pop()
-Pop()
+def learnStack():
+    print(a)
+    Push(5)  # Push and Pop operation
+    Push(6)
+    Push(27)
+    Pop()
+    Pop()
+    print(a)  # Print the updated list after all operations
 
-print(a)  # Print the updated list after all operations
-# =============================================================#
+
+# ============================================================#
+
+learnStack()
+
